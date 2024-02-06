7e6fc9191cf96d4a4d6d173d77ee78a1a4f1452b
cadyn d <ducc11507@student.trentonschools.com>
2024-02-06 17:58:05 -0500

    LED change colors when it picks u an object

---
+++ src/main/java/frc/robot/commands/LEDControlCommand.java
@@ -0,0 +1,23 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class LEDControlCommand extends Command {
+    
+    @Override
+    public void initialize() {
+    // This command should turn on the LED at the start of the game
+
+    }
+    
+    @Override
+    public void execute() {
+     // This command should change colors fro if there is an object
+
+    }
+    @Override
+    public boolean isFinished(){
+        // This command should turn off the LED at the end of the game
+        return false;
+    }
+}
