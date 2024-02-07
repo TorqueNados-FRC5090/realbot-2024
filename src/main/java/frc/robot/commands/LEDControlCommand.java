e81e90c63d313b0436171f2912e77263a1dbc88e
cadyn d <ducc11507@student.trentonschools.com>
2024-02-07 17:19:29 -0500

    we are telling the robot to change the color of the LED

--- src/main/java/frc/robot/commands/LEDControlCommand.java
+++ src/main/java/frc/robot/commands/LEDControlCommand.java
@@ -1,19 +1,31 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Blinkin;
+import frc.robot.subsystems.Intake;
 
 public class LEDControlCommand extends Command {
-    
+    Intake intake;
+    Blinkin blinkin;
+    public LEDControlCommand(Intake intake, Blinkin blinkin) {
+       this.intake = intake;
+       this.blinkin = blinkin;
+    }
+
     @Override
     public void initialize() {
     // This command should turn on the LED at the start of the game
-
+        
     }
     
     @Override
     public void execute() {
      // This command should change colors fro if there is an object
+        if (intake.holdingPiece()) {
+            blinkin.blueSolid(); }
+        else blinkin.goldSolid();
 
+        
     }
     @Override
     public boolean isFinished(){
